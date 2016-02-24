468e285f1009c03211575ed6368ad69ce397c332
GuyWithTheGlasses <kevin.a.yan@gmail.com>
2016-02-23 20:54:47 -0500

    tfw everything works as expected

--- hw03/animate.js
+++ hw03/animate.js
@@ -59,28 +59,40 @@ var stopCircle = function stopCircle(){
 var stopbtn = document.getElementById("stop");
 stopbtn.addEventListener("click",stopCircle);
 
+/*------------------------ Random Number Generator ------------------------*/
+
+//Found this code online
+//"Returns a random integer between min (inclusive) and max (inclusive)"
+//Thanks stackoverflow
+var getRandomInt = function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 /*---------------------------- DVD Bouncing -------------------------------*/
 
-//Initialixe current x and y coordinates
-var xcor = c.width/2;
-var ycor = c.height/2;
-//Initialize x and y velocities
+//Set x and y velocities 
 var xvel = 1;
 var yvel = 1;
+//Set image width and height
+var imgW = 100;
+var imgH = 50;
+//Initialize current x and y coordinates
+var xcor = getRandomInt(0,c.width-imgW);
+var ycor = getRandomInt(0,c.height-imgH);
 
 //Make DVD move and bounce around the canvas
-var bounceDVD = function bounceDVD(e){
+var bounceDVD = function bounceDVD(){
     //Increment the x and y coords
     xcor = xcor + xvel;
     ycor = ycor + yvel;
     //Negate the velocities if we hit extreme values
-    if(xcor > c.width || xcor <= 0){
+    if(xcor <= 0 || xcor+imgW > c.width){
 	xvel = -1*xvel;
-    } else if (ycor > c.height || ycor <= 0){
+    } else if (ycor <= 0 || ycor+imgH > c.height){
 	yvel = -1*yvel;
     }
     //Actually draw the image
-    ctx.drawImage(logo,xcor,ycor,100,50);
+    ctx.drawImage(logo,xcor,ycor,imgW,imgH);
     //Call function again
     requestID = window.requestAnimationFrame(bounceDVD);
 };
