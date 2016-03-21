86cddb1bac8cb6ec74449cf7d1837141cb9aa678
GuyWithTheGlasses <kevin.a.yan@gmail.com>
2016-03-20 22:21:03 -0400

    Total delegates pledged done

--- hw05/bar.js
+++ hw05/bar.js
@@ -19,56 +19,59 @@ d3.select(".chart")
     });
 */
 
-//Within each entry: data[1] = Trump, data[2] = Cruz, data[3] = Rubio, data[4] = Kasich
-var repdata = [
-    {state:"Iowa", data:[7,8,7,1]},
-    {state:"New Hampshire", data:[11,3,2,4]}, 
-    {state:"South Carolina", data:[50,0,0,0]}, 
-    {state:"Nevada", data:[14,6,7,1]}, 
-    {state:"Alabama", data:[36,13,1,0]}, 
-    {state:"Alaska", data:[11,12,5,0]}, 
-    {state:"Arkansas", data:[16,14,9,0]}, 
-    {state:"Georgia", data:[40,18,14,0]}, 
-    {state:"Massachusetts", data:[22,4,8,8]}, 
-    {state:"Minnesota", data:[8,13,17,0]}, 
-    {state:"Oklahoma", data:[13,15,12,0]}, 
-    {state:"Tennesee", data:[33,16,9,0]}, 
-    {state:"Texas", data:[48,104,3,0]}, 
-    {state:"Vermont", data:[8,0,0,8]}, 
-    {state:"Virginia", data:[17,8,16,5]}, 
-    {state:"Kansas", data:[9,24,6,1]}, 
-    {state:"Kentucky", data:[17,15,7,7]}, 
-    {state:"Louisiana", data:[18,18,5,0]}, 
-    {state:"Maine", data:[9,12,0,2]}, 
-    {state:"Puerto Rico", data:[0,0,23,0]}, 
-    {state:"Hawaii", data:[11,7,1,0]}, 
-    {state:"Idaho", data:[12,20,0,0]}, 
-    {state:"Michigan", data:[25,17,0,17]}, 
-    {state:"Mississippi", data:[24,13,0,0]}, 
-    {state:"District of Columbia", data:[0,0,10,9]}, 
-    {state:"Wyoming", data:[1,9,1,0]}, 
-    {state:"Florida", data:[99,0,0,0]}, 
-    {state:"Illinois", data:[51,9,0,5]}, 
-    {state:"Missouri", data:[25,5,0,0]}, 
-    {state:"North Carolina", data:[29,27,6,9]}, 
-    {state:"Northern Mariana Islands", data:[9,0,0,0]}, 
-    {state:"Ohio", data:[0,0,0,66]}, 
-    {state:"Superdelegates", data:[0,1,0,0]}
-]
-
-var chart = d3.select("chart");
-var bar = chart.selectAll("div");
-var barUpdate = bar.data(repdata, function(d){
-    return d.state;
-});
-var barEnter = barUpdate.enter().append("div");
+//Within each entry: data[0] = Trump, data[1] = Cruz, data[2] = Rubio, data[3] = Kasich
+var repdata = {
+    "Iowa":[7,8,7,1],
+    "New Hampshire":[11,3,2,4],
+    "South Carolina":[50,0,0,0], 
+    "Nevada":[14,6,7,1], 
+    "Alabama":[36,13,1,0], 
+    "Alaska":[11,12,5,0], 
+    "Arkansas":[16,14,9,0], 
+    "Georgia":[40,18,14,0], 
+    "Massachusetts":[22,4,8,8], 
+    "Minnesota":[8,13,17,0], 
+    "Oklahoma":[13,15,12,0], 
+    "Tennesee":[33,16,9,0], 
+    "Texas":[48,104,3,0], 
+    "Vermont":[8,0,0,8], 
+    "Virginia":[17,8,16,5], 
+    "Kansas":[9,24,6,1], 
+    "Kentucky":[17,15,7,7], 
+    "Louisiana":[18,18,5,0], 
+    "Maine":[9,12,0,2], 
+    "Puerto Rico":[0,0,23,0], 
+    "Hawaii":[11,7,1,0], 
+    "Idaho":[12,20,0,0], 
+    "Michigan":[25,17,0,17], 
+    "Mississippi":[24,13,0,0], 
+    "District of Columbia":[0,0,10,9], 
+    "Wyoming":[1,9,1,0], 
+    "Florida":[99,0,0,0], 
+    "Illinois":[51,9,0,5], 
+    "Missouri":[25,5,0,0], 
+    "North Carolina":[29,27,6,9], 
+    "Northern Mariana Islands":[9,0,0,0], 
+    "Ohio":[0,0,0,66]
+}
+var states = Object.keys(repdata);
+console.log(states);
 
 var foo = d3.scale.linear()
-    .domain([0,d3.max(repdata)])
+    .domain([0,200])
     .range([0,1920]);
-barEnter.style("width",function(d){
-    return foo(d) + "px";
-});
-console.log(barEnter);
+
+d3.select(".chart")
+    .selectAll("div")
+  .data(states)
+    .enter().append("div")
+  .style("width", function(d){
+      var dies = repdata[d][0] + repdata[d][1] + repdata[d][2] + repdata[d][3];
+      return foo(dies) + "px";
+  })
+  .text(function(d){
+      return d;
+  });
+
 
 
