e9fadfc925c9fffaabf8348a0e2c260798278b3d
GuyWithTheGlasses <kevin.a.yan@gmail.com>
2016-04-04 12:27:34 -0400

    Stuff

--- hw06/list.py
+++ hw06/list.py
@@ -30,6 +30,7 @@ print [1 if x in UC_LETTERS else 0 for x in p]
 UC_LETTERS="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 LC_LETTERS="abcdefghijklmnopqrstuvwxyz"
 NUMBERS="0123456789"
+NON-ALPHANUM= "!@#&-:;',?/*" 
 
 #Check whether password meets a minimum threshold:
 #Contains mixture of upper and lowercase letters, and at least 1 number
@@ -44,3 +45,52 @@ print pass_check("Hey1tsm3")
 
 #Return a password's strength rating, integer scale of 1-10
 #Rating depends on use of numbers, ucase, lcase, and non-alphanumeric chars
+def pass_strength(pw):
+    ucase = [1 if x in UC_LETTERS else 0 for x in pw] 
+    lcase = [1 if x in LC_LETTERS else 0 for x in pw]
+    number = [1 if x in NUMBERS else 0 for x in pw]
+    batman = [1 if x in NON-ALPHANUM else 0 for x in pw]
+    strength = 0
+
+    if not pass_check(pw):
+        return 1
+
+    if sum(ucase) >= 4:
+        strength += 4
+    else:
+        strength += sum(ucase)
+
+    if sum(lcase) >= 4:
+        strength += 4
+    else:
+        strength += sum(lcase)
+
+    if sum(number) >= 1:
+        strength += 1
+    else:
+        strength += sum(number)
+
+    if sum(batman) >= 1:
+        strength += 1
+    else:
+        strength += sum(batman)
+
+    return strength
+
+def strength_check(pw):
+    l = [1 if x in UC_LETTERS else
+         2 if x in LC_LETTERS else
+         3 if x in NUMBERS else
+         0 for x in p]
+    return 1 in l and 2 in l and 3 in l
+
+p1 = "password"
+p2 = "passw0rd"
+p3 = "Password"
+p4 = "Passw0rd"
+
+print p1 + ": " + str(strength_check(p1))
+print p2 + ": " + str(strength_check(p2))
+print p3 + ": " + str(strength_check(p3))
+print p4 + ": " + str(strength_check(p4))
+
